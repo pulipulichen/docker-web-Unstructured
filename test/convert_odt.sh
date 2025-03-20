4a6e5e5137b507c5c4667e6a6456f76bd5485edd
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-20 23:35:30 +0800

    西元2025年03月20日 (週四) 23時35分30秒 CST

--- test/convert_odt.sh
+++ test/convert_odt.sh
@@ -1,1 +1,7 @@
-# 將 input.odt 傳送給 192.168.100.
+#!/bin/bash
+# 使用 curl 命令將 test/input.odt 檔案傳送到 192.168.100.213
+# 20250320-233435
+
+cd $(dirname "$0")
+
+curl -F "file=@input.odt" http://192.168.100.213/
