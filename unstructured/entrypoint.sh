ccfd04db862ee6550bcd07c51d406552955beee2
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-20 23:40:16 +0800

    西元2025年03月20日 (週四) 23時40分16秒 CST

--- unstructured/entrypoint.sh
+++ unstructured/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# uvicorn app:app --host 0.0.0.0 --port 8080 --workers ${UVICORN_WORKERS}
-uvicorn app:app --host 0.0.0.0 --port 8080 --workers 1
-# uvicorn app:app --host 0.0.0.0 --port 8080 --reload
+uvicorn app:app --host 0.0.0.0 --port 80 --workers 1
