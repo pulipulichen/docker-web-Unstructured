616219eb18f24a606b370c7429deb7698f871e9e
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-03-20 23:46:48 +0800

    西元2025年03月20日 (週四) 23時46分48秒 CST

--- unstructured/lib/check_convert.py
+++ unstructured/lib/check_convert.py
@@ -7,7 +7,7 @@ def check_convert(file_path):
     把 file_path 用 post 傳送到 converter-libreoffice ，然後取得回覆裡面的 file_path 並return
     20250320-233000
     """
-    converter_url = os.getenv("CONVERTER_LIBREOFFICE_URL", "http://converter_libreoffice:8000") # 從環境變數取得 converter-libreoffice 的 URL，如果沒有設定，預設為 http://converter_libreoffice:8000
+    converter_url = os.getenv("CONVERTER_LIBREOFFICE_URL", "http://converter_libreoffice:80") # 從環境變數取得 converter-libreoffice 的 URL，如果沒有設定，預設為 http://converter_libreoffice:8000
     try:
         response = requests.post(converter_url, data={"file_path": file_path}) # 發送 POST 請求，將 file_path 作為 data 傳送
         response.raise_for_status()  # 如果回覆狀態碼不是 200，就拋出異常
